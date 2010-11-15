75dce48fe1821547286e0efcd36ff85c7a8011e8
Nate Weiner <acc@ideashower.com>
2010-11-15 12:06:49 -0800

    Fixing merge issues

--- Classes/ShareKit/SHKConfig.h
+++ Classes/ShareKit/SHKConfig.h
@@ -2,7 +2,6 @@
 
 
 
-
 // PLEASE SEE INSTALL/CONFIG INSTRUCTIONS:
 // http://getsharekit.com/install
 
@@ -20,47 +19,50 @@
 
 
 /*
- API Keys
- --------
- This is the longest step to getting set up, it involves filling in API keys for the supported services.
- It should be pretty painless though and should hopefully take no more than a few minutes.
+API Keys
+--------
+This is the longest step to getting set up, it involves filling in API keys for the supported services.
+It should be pretty painless though and should hopefully take no more than a few minutes.
  
- Each key below as a link to a page where you can generate an api key.  Fill in the key for each service below.
+Each key below as a link to a page where you can generate an api key.  Fill in the key for each service below.
  
- A note on services you don't need:
- If, for example, your app only shares URLs then you probably won't need image services like Flickr.
- In these cases it is safe to leave an API key blank.
+A note on services you don't need:
+If, for example, your app only shares URLs then you probably won't need image services like Flickr.
+In these cases it is safe to leave an API key blank.
  
- However, it is STRONGLY recommended that you do your best to support all services for the types of sharing you support.
- The core principle behind ShareKit is to leave the service choices up to the user.  Thus, you should not remove any services,
- leaving that decision up to the user.
- */
-
+However, it is STRONGLY recommended that you do your best to support all services for the types of sharing you support.
+The core principle behind ShareKit is to leave the service choices up to the user.  Thus, you should not remove any services,
+leaving that decision up to the user.
+*/
 
 
 // Delicious - https://developer.apps.yahoo.com/projects
-#define SHKDeliciousConsumerKey		@""
-#define SHKDeliciousSecretKey		@""
+#define SHKDeliciousConsumerKey		@"dj0yJmk9bHJJQTN4ajFOVkU4JmQ9WVdrOWVqSjJiMng1TjJVbWNHbzlNamc1T1RnM05qWXkmcz1jb25zdW1lcnNlY3JldCZ4PTgx"
+#define SHKDeliciousSecretKey		@"5a2be05d7508f3be9ba4f7b67b95ec9dd4feb2e9"
+
 
 // Evernote - http://www.evernote.com/about/developer/api/
-#define SHKEvernoteConsumerKey @""
-#define SHKEvernoteSecretKey @""
+#define SHKEvernoteConsumerKey @"readitlater"
+#define SHKEvernoteSecretKey @"c4faf84d7c4b958c"
 #define SHKEvernoteUserStoreURL @"https://sandbox.evernote.com/edam/user"
 #define SHKEvernoteNetStoreURLBase @"http://sandbox.evernote.com/edam/note/"
 // If your Consumer Key is activated by Evernote support, change these constants following:
 //#define SHKEvernoteUserStoreURL @"https://www.evernote.com/edam/user"
 //#define SHKEvernoteNetStoreURLBase @"http://www.evernote.com/edam/note/"
 
+
 // Facebook - http://www.facebook.com/developers
 // If SHKFacebookUseSessionProxy is enabled then SHKFacebookSecret is ignored and should be left blank
 
 #define SHKFacebookUseSessionProxy  NO 
-#define SHKFacebookKey				@""
-#define SHKFacebookSecret			@""
+#define SHKFacebookKey				@"865df42c04f42f95ba8b6775d9aba67a"
+#define SHKFacebookSecret			@"ff527ecdb5c2f3081b04824cce991c11"
 #define SHKFacebookSessionProxyURL  @""
 
-// Read It Later - http://readitlaterlist.com/api/?shk
-#define SHKReadItLaterKey			@""
+
+// Read It Later - http://readitlaterlist.com/api/
+#define SHKReadItLaterKey			@"0Z0A5y05deD94k74d6p4960N9eT1BF6a" // (optional) : Recommended, apps using keys are featured in the RIL app directory and can track usage.
+
 
 // Twitter - http://dev.twitter.com/apps/new
 /*
@@ -77,25 +79,27 @@
  1. Open your application settings at http://dev.twitter.com/apps/
  2. 'Application Type' should be set to BROWSER (not client)
  3. 'Callback URL' should match whatever you enter in SHKTwitterCallbackUrl.  The callback url doesn't have to be an actual existing url.  The user will never get to it because ShareKit intercepts it before the user is redirected.  It just needs to match.
- */
-#define SHKTwitterConsumerKey		@""
-#define SHKTwitterSecret			@""
-#define SHKTwitterCallbackUrl		@"" // You need to set this if using OAuth, see note above (xAuth users can skip it)
+*/
+#define SHKTwitterConsumerKey		@"Q3EvVAsxIj6cXymHJeTZNw"
+#define SHKTwitterSecret			@"6JvXlx92AI07kcgqQzrWN7hOhubHrvWEYUuhcs7DE"
+#define SHKTwitterCallbackUrl		@"http://readitlaterlist.com/oauthcallback" // HOW-TO: In your Twitter application settings, use the "Callback URL" field.  If you do not have this field in the settings, set your application type to 'Browser'.
 #define SHKTwitterUseXAuth			0 // To use xAuth, set to 1
-#define SHKTwitterUsername			@"" // Enter your app's twitter account if you'd like to ask the user to follow it when logging in. (Only for xAuth)
+#define SHKTwitterUsername			@"ReadItLater" // Enter your app's twitter account if you'd like to ask the user to follow it when logging in. (Only for xAuth)
+
 
 // Bit.ly (for shortening URLs on Twitter) - http://bit.ly/account/register - after signup: http://bit.ly/a/your_api_key
-#define SHKBitLyLogin				@""
-#define SHKBitLyKey					@""
+#define SHKBitLyLogin				@"readitlater"
+#define SHKBitLyKey					@"R_01ef88f67ae36d11f80119810a65ddb9"
+
+
+
 
 
 /*
  Translations
- */
+*/
 #warning Read this important note on included translations
 // Read http://getsharekit.com/translations and then you can remove this warning
-
-
 
 
 
@@ -103,7 +107,7 @@
  UI Configuration : Basic
  ------
  These provide controls for basic UI settings.  For more advanced configuration see below.
- */
+*/
 
 // Toolbars
 #define SHKBarStyle					@"UIBarStyleDefault" // See: http://developer.apple.com/iphone/library/documentation/UIKit/Reference/UIKitDataTypesReference/Reference/reference.html#//apple_ref/c/econst/UIBarStyleDefault
@@ -128,14 +132,16 @@
 #define SHKShareMenuAlphabeticalOrder 1 // Setting this to 1 will show list in Alphabetical Order, setting to 0 will follow the order in SHKShares.plist
 
 // Append 'Shared With 'Signature to Email (and related forms)
-#define SHKSharedWithSignature		0
+#define SHKSharedWithSignature		1
+
 
 /*
  UI Configuration : Advanced
  ------
  If you'd like to do more advanced customization of the ShareKit UI, like background images and more,
  check out http://getsharekit.com/customize
- */
+*/
+
 
 
 
@@ -149,24 +155,25 @@
  To hide debug output in the console:
  1. uncomment section B below
  2. comment out section A below
- */
+*/
 
 // A : show debug output
-//#define SHKDebugShowLogs			1
-//#define SHKLog( s, ... ) NSLog( @"<%p %@:(%d)> %@", self, [[NSString stringWithUTF8String:__FILE__] lastPathComponent], __LINE__, [NSString stringWithFormat:(s), ##__VA_ARGS__] )
+#define SHKDebugShowLogs			1
+#define SHKLog( s, ... ) NSLog( @"<%p %@:(%d)> %@", self, [[NSString stringWithUTF8String:__FILE__] lastPathComponent], __LINE__, [NSString stringWithFormat:(s), ##__VA_ARGS__] )
 
 // B : hide debug output
-#define SHKDebugShowLogs			0
-#define SHKLog( s, ... ) 
+//#define SHKDebugShowLogs			0
+//#define SHKLog( s, ... ) 
 
 
 
 /*
- Advanced Configuration
+Advanced Configuration
  ------
- These settings can be left as is.  This only need to be changed for uber custom installs.
- */
+These settings can be left as is. 
+*/
 
 #define SHK_MAX_FAV_COUNT			3
 #define SHK_FAVS_PREFIX_KEY			@"SHK_FAVS_"
 #define SHK_AUTH_PREFIX				@"SHK_AUTH_"
+
